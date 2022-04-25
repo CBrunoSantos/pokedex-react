e719af7c9151ed4719db67ac963c7719fefa5030
CBrunoSantos <brunosantos@edu.unifor.br>
2022-04-25 16:29:41 -0300

    finalizando o grid da pokedex

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
-// import "./Pokedex.css";
+import "./Pokemon.css";
+import heart from '../Pokemon/like.png'
 
 const Pokemon = (props) => {
 
     const {pokemon} = props
-  return (
+    const onHeartClick = () => {
+        console.log("favoritado");
+    }
+
+    return (
     <div className="pokemon-card">
         <div className="pokemon-img-container">
         <img className="pokemon-img" src={pokemon.sprites.front_default}/>
@@ -12,7 +17,10 @@ const Pokemon = (props) => {
         <div className="pokemon-body">
             <div className="pokemon-topo">
                 <h3>{pokemon.name}</h3>
-                <div>{pokemon.id}</div>
+                <div>#{pokemon.id}</div>
+                <button className="pokemon-heart-btn" onClick={onHeartClick}>
+                <img src={heart}/>
+            </button>
             </div>
             <div className="pokemon-footer">
             <div className="pokemon-tipo">
@@ -22,7 +30,6 @@ const Pokemon = (props) => {
                     )
                 })}
             </div>
-            <button className="pokemon-heart-btn"></button>
             </div>
         </div>
         
