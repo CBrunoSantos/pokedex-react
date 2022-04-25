3582f5fa74505ff2a2c229a97883a9618984ba94
CBrunoSantos <brunosantos@edu.unifor.br>
2022-04-25 11:38:56 -0300

    Pokedex-react

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar-container"><img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"/></div>
+    </nav>
+  );
+};
+
+export default Navbar;
